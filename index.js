d4f04a53f70fe605df735ae623c5a3aeb8ba8ecb
Reni98 <93825160+Reni98@users.noreply.github.com>
2023-05-09 11:19:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let navigation = document.querySelector(".navigation");
+let menuToggle = document.querySelector(".menuToggle");
+let header = document.querySelector("header");
+
+menuToggle.onclick = function () {
+  header.classList.toggle("open");
+};
